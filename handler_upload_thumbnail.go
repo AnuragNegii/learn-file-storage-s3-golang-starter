965a4c3b0aa884e4bfd7bba7669b33c3d1373cbc
Anurag Negi <neginaurag99@gmail.com>
2025-08-06 23:09:53 +0530

    cache check

--- handler_upload_thumbnail.go
+++ handler_upload_thumbnail.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"io"
+	"mime"
 	"net/http"
 	"os"
 	"path/filepath"
@@ -47,7 +48,6 @@ func (cfg *apiConfig) handlerUploadThumbnail(w http.ResponseWriter, r *http.Requ
 		return
 	}
 	defer file.Close()	
-	mediaType := header.Header.Get("Content-Type")
 	video, err := cfg.db.GetVideo(videoID)
 	if err!=nil{
 		respondWithError(w, http.StatusBadRequest, "couldn't get thumbnail file", err)
@@ -57,8 +57,13 @@ func (cfg *apiConfig) handlerUploadThumbnail(w http.ResponseWriter, r *http.Requ
 		respondWithError(w, http.StatusUnauthorized, "you are not authorized for this", nil)
 		return
 	}
+	fileString, _, err := mime.ParseMediaType(header.Header.Get("Content-Type"))
+	if err != nil{
+		respondWithError(w, http.StatusBadRequest, "couldn't get the file type from the file", nil)
+		return
+	}
 	var fileType string
-	switch mediaType {
+	switch fileString{
 	case "image/jpeg":
 		fileType = ".jpg"	
 	case "image/png":
@@ -67,7 +72,6 @@ func (cfg *apiConfig) handlerUploadThumbnail(w http.ResponseWriter, r *http.Requ
 		respondWithError(w, http.StatusBadRequest, "wrong file type submitted", nil)
 		return
 	}
-	
 	videoFile := fmt.Sprintf("%s%s",video.ID.String(),fileType)
 	newPath := filepath.Join(cfg.assetsRoot,videoFile)
 	dst, err := os.Create(newPath)	
@@ -81,7 +85,7 @@ func (cfg *apiConfig) handlerUploadThumbnail(w http.ResponseWriter, r *http.Requ
 		respondWithError(w, http.StatusBadRequest, "couldn't read the file", err)
 		return
 	}
-	publicUrl := "/assets/" + videoFile
+	publicUrl := "http://localhost:8091/assets/" + videoFile
 	video.ThumbnailURL = &publicUrl
 	err = cfg.db.UpdateVideo(video)
 	if err != nil{
